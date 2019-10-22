619be831f741e760fea0bd4554f1a4f1f1a7bb3d
Alaa_Serhal <alaa.serhal@outlook.com>
2019-10-23 00:49:08 +0300

    User Input
    User inputs the number of rows and columns he wants

--- ui.py
+++ ui.py
@@ -7,25 +7,27 @@ WIDTH = 70
 HEIGHT = 70
 MARGIN = 1.5
 grid = []
-for row in range(7):
+input = input("Enter your input : ")
+g= int(input)
+for row in range(g):
     grid.append([])
-    for column in range(7):
+    for column in range(g):
         grid[row].append(0)
 pygame.init()
-WINDOW_SIZE = [500, 500]
+WINDOW_SIZE = [round(g*71.42), round(g*71.42)]
 screen = pygame.display.set_mode(WINDOW_SIZE)
 pygame.display.set_caption("Vaccum Cleaner Agent")
 done = False
 clock = pygame.time.Clock()
 while not done:
-    for event in pygame.event.get():  
-        if event.type == pygame.QUIT:  
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
             done = True
         elif event.type == pygame.MOUSEBUTTONDOWN:
             pos = pygame.mouse.get_pos()
     screen.fill(BLACK)
-    for row in range(7):
-        for column in range(7):
+    for row in range(g):
+        for column in range(g):
             color = WHITE
             if grid[row][column] == 1:
                 color = GREEN
