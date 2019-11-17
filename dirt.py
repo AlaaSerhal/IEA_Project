03b0d1e6af5e97ee354385e84f1330ccd97bf4c0
mario <mario.mokbel0@gmail.com>
2019-11-17 22:27:47 +0200

    bug fix

--- dirt.py
+++ dirt.py
@@ -4,7 +4,7 @@ from room import room
 from math import hypot
 
 class dirt:
-    def __init__(self, room, window, initial_number):
+    def __init__(self, room, window, initial_number=6):
         self.room = room
         self.window = window
         self.initial_number = initial_number
@@ -60,7 +60,7 @@ class dirt:
         while(i < number and count < 10):
             col = random.randint(0, self.room.get_cols()-1)
             row = random.randint(0, self.room.get_rows()-1)
-            
+
             if(self.room.get_array()[row][col].has_dirt() or self.room.get_array()[row][col].has_vacuum()):
                 count += 1
             else:
